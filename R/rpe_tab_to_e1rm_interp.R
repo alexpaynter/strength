094dcad2abca298fb80fce1789de5d1864d2721f
Alex Paynter <alexcpaynter@gmail.com>
2026-02-06 09:48:05 -0800

    Added function/data for rpe tab to lookup conversion

--- R/rpe_tab_to_e1rm_interp.R
+++ R/rpe_tab_to_e1rm_interp.R
@@ -9,16 +9,6 @@ rpe_tab_to_e1rm_interp <- function(rpe_tab) {
     dplyr::mutate(reps = as.numeric(reps),
                   pct_1rm = as.numeric(pct_1rm))
 
-  e1rm_interp <- e1rm_interp |>
-    dplyr::arrange(reps, desc(rpe)) |>
-    dplyr::group_by(reps) |>
-    dplyr::mutate(
-      next_rpe_down = dplyr::lead(rpe),
-      next_pct_1rm_down = dplyr::lead(pct_1rm)
-    ) |>
-    dplyr::select(reps, rpe, next_rpe_down, pct_1rm, next_pct_1rm_down) |>
-    dplyr::ungroup()
-
   e1rm_interp
 
 }
