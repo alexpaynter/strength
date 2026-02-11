42575423a0a6ad9fa8365ed09212391d39b744f4
Alex Paynter <alexcpaynter@gmail.com>
2026-02-11 10:02:01 -0800

    Created estimated one rep max functions

---
+++ R/e1rm.R
@@ -0,0 +1,20 @@
+#' Estimated one rep max
+#'
+#' @param weight Numeric vector of weights, one entry per set.
+#' @param reps Numeric vector of reps, one entry per set.
+#' @param rpe Numeric vector of RPEs, one entry per set.
+#' @param pct1rm_func A function which takes `reps`,`rpe` and returns an estimated percentage of one rep max.
+#'
+#' @returns A numeric vector of estimated one rep max values for each set.
+#' @export
+#'
+#' @examples
+e1rm <- function(weight, reps, rpe, pct1rm_func) {
+  p1rm <- pct1rm_func(reps, rpe)
+  weight / (p1rm / 100)
+}
+
+e1rm_rts <- function(...) {
+  dots <- list(...)
+  e1rm(..., pct1rm_rts)
+}
