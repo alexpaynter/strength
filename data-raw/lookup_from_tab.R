64e62bf48e267fd9c4ee6d1ab3eb969a2a7a62d8
Alex Paynter <alexcpaynter@gmail.com>
2026-02-10 23:55:04 -0800

    Added helms RPE/RIR equations

---
+++ data-raw/lookup_from_tab.R
@@ -0,0 +1,11 @@
+rpe_tab_rts <- readr::read_csv(
+  'data-raw/rpe_tab_rts.csv'
+)
+pct_lookup_rts <- rpe_tab_to_lookup(rpe_tab_rts)
+usethis::use_data(pct_lookup_rts, overwrite = TRUE)
+
+rep_tab_helms <- readr::read_csv(
+  'data-raw/rpe_tab_helms.csv'
+)
+pct_lookup_helms <- rpe_tab_to_lookup(rpe_tab_helms)
+usethis::use_data(pct_lookup_helms, overwrite = TRUE)
--- data-raw/pct_lookup_rts.R
+++
@@ -1,7 +0,0 @@
-rpe_tab_rts <- readr::read_csv(
-  'data-raw/rpe_tab_rts.csv'
-)
-
-pct_lookup_rts <- rpe_tab_to_lookup(rpe_tab_rts)
-
-usethis::use_data(pct_lookup_rts, overwrite = TRUE)
