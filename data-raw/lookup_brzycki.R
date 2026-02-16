201055216dfa8e944ad47ca358720b31514cfd61
Alex Paynter <alexcpaynter@gmail.com>
2026-02-15 21:48:18 -0800

    added a stub for brzycki formulas

---
+++ data-raw/lookup_brzycki.R
@@ -0,0 +1,9 @@
+# ISBN for the book where this was introduced: 978-1935628132
+
+# Formula is 1rm = weight * 36 / (37 - r).
+
+# This gives you the 10 RPE row.
+
+# Plan to extend this to RPE scales is:  a 1 rep @ 9 = 2 @ 10.  1 rep @ 8 = 2 @ 9 = 3 @ 10, etc.  The you can interpolate to find 9.5, 8.5, etc.  I think this is a solid plan actually.
+
+# I'll probably cap the table at something like 20 reps.  The actual suggestion may have been to go lower.
